af67bd94192f55b608d861951ac4e75d8067b70f
Hojjat Aghakhani <Hojjat.aghakhani72@gmail.com>
2015-07-04 21:30:51 +0430

    project done

---
+++ index.js
@@ -0,0 +1,268 @@
+/**
+ * Created by hojjat on 6/29/15 AD.
+ */
+var ObjectId = require('mongodb').ObjectID;
+
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('/usr/local/lib/node_modules/socket.io')(server);
+var port = process.env.PORT || 3000;
+
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/chat';
+
+var database;
+
+var sockets = [];
+
+// here we define collections
+var users;
+var messages;
+
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        database = db;
+        console.log('Connection established to', url);
+
+        // here we define collections
+        users = database.collection('users');
+        messages = database.collection('messages');
+    }
+});
+
+// setting views directory
+app.set('views', __dirname + '/views');
+
+// setting view engine
+app.set('view engine', 'jade');
+
+// setting static folders, for js, css and images file!
+app.use(express.static(path.join(__dirname, 'public')));
+
+// server listen on port! please attention, server listen for http request!
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// server respond to browser connection(127.0.0.1:port/), a http connection
+app.get('/', function (req, res) {
+    res.render('index.jade', {
+    });
+});
+
+// here we have web socket connection, not http connections
+io.on('connection', function (socket) {
+    socket.emit('signin-req', {});
+    console.log("user connected");
+
+    socket.on('disconnect', function (data) {
+        var username = socket.username;
+        if(socket.username === undefined){
+            return;
+        }
+        users.find({username: username}).toArray(function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else if (result.length) {
+                if (result[0] !== undefined)
+                    sendOnlineMessageToFriends(result[0].friends, result[0].username, 'notonline');
+            }
+            else {
+            }
+        });
+    });
+
+
+    // when the server receive the user info!
+    socket.on('signup', function (data) {
+        var username = data.username;
+        socket.username = username;
+        var name = data.name;
+        // Get the documents collection
+        if (username === '')
+            socket.emit('signup-status', {status: "Please enter a username"});
+        var u = {username: username, name: name, friends: [], lastSeen: new Date()};
+
+        users.find({username: username}).toArray(function (err, result) {
+            if (err) {
+                console.log(err);
+            } else if (result.length) {
+                socket.emit('signup-status', {status: "The username already exists"});
+            } else {
+                users.insert(u, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        socket.username = username;
+                        sockets[username] = socket;
+                        socket.emit('signup-status', {status: "success", username: username, name: name});
+                    }
+                });
+            }
+        });
+    });
+
+    // when the client tries to sign in!
+    socket.on('signin', function (data) {
+        var username = data.username;
+        if (username === '')
+            socket.emit('signin-status', {status: "Please enter a username"});
+        users.find({username: username}).toArray(function (err, result) {
+            if (err) {
+                console.log(err);
+            } else if (result.length) {
+                socket.username = username;
+                sockets[username] = socket;
+                var cursor = messages.find({ $or: [
+                    {from: username},
+                    {to: username}
+                ]}).sort({dateTime: 1});
+                socket.emit('signin-status', {status: "success", user: result});
+                sendOnlineMessageToFriends(result[0].friends, result[0].username, 'online');
+                cursor.each(function (err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        if (doc !== null)
+                            socket.emit('messages', {message: doc});
+                    }
+                });
+            } else {
+                socket.emit('signin-status', {status: "Please enter a valid username!"});
+            }
+        });
+    });
+
+    socket.on('online-ack', function(data){
+        var username = data.username;
+        var s = findSocket(username);
+        s.emit('online-ack', {username: socket.username});
+    });
+
+    // when the user send a friend name to server
+    socket.on('find-friend', function (data) {
+        var friendUsername = data.friend;
+        users.find({username: friendUsername}).toArray(function (err, result) {
+            if (err) {
+                console.log(err);
+            } else if (result.length) {
+                if (!search(result[0].friends, socket.username)) {
+                    users.update({ username: socket.username }, { $push: { friends: result[0].username } }, function (err, res) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            users.update({ username: result[0].username }, { $push: { friends: socket.username } }, function (err, res) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                else {
+                                    socket.emit('find-friend-status', {status: "success", user: result[0]});
+                                    var s = findSocket(result[0].username);
+                                    if (s !== undefined){
+                                        s.emit('online', {username: socket.username});
+                                        s.emit('friend', {status: "success", friend: socket.username});
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            } else {
+                socket.emit('find-friend-status', {status: "Please enter a valid username!"});
+            }
+        });
+    });
+
+    socket.on('seen-req', function (data) {
+        var username = data.username;
+        users.find({username: username}).toArray(function (err, res) {
+            if (err) {
+                console.log(err);
+            }
+            else if (res.length) {
+                socket.emit('seen-ack', {username: username, lastSeen: res[0].lastSeen});
+            }
+            else {
+            }
+        });
+    });
+
+    socket.on('seen', function (data) {
+        var id = data.id;
+        var receiver = data.receiver;
+        messages.update({_id: ObjectId(id)}, {$set: {seen: 1}}, function (err, res) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                users.update({receiver: receiver}, {$set: {lastSeen: new Date()}}, function (err, res) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                    }
+                });
+            }
+        });
+    });
+
+    socket.on('message', function (data) {
+        var from = socket.username;
+        var to = data.friend;
+        var message = data.text;
+        var dateTime = new Date();
+        var m = {from: from, to: to, message: message, dateTime: dateTime, seen: 0};
+        messages.insert(m, function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                socket.emit('messages', {status: "success", message: m});
+                sendMessageToUser(to, m);
+            }
+        });
+    });
+
+    var sendOnlineMessageToFriends = function (friends, username, status) {
+        var l = friends.length;
+        for (var i = 0; i < l; i++) {
+            var s = findSocket(friends[i]);
+            if (s !== undefined)
+                s.emit(status, {'username': username});
+        }
+    };
+
+    var sendMessageToUser = function (username, message) {
+        var s = findSocket(username);
+        if (s !== undefined)
+            s.emit('messages', {status: "success", message: message});
+    };
+
+    var findSocket = function (username) {
+        return sockets[username];
+    };
+
+    var search = function (list, element) {
+        var l = list.length;
+        for (var i = 0; i < l; i++) {
+            if (list[i] === element)
+                return true;
+        }
+        return false;
+    };
+});
+
---
+++ public/static/js/client.js
@@ -0,0 +1,313 @@
+$(function () {
+    $("#message-pane").hide();
+    var socket = io.connect();
+    var messages = [];
+    var USERNAME;
+    socket.on('signin-req', function (data) {
+        $('.ui.modal')
+            .modal('setting', 'closable', false)
+            .modal('show')
+        ;
+        console.log("hi");
+        messages = [];
+        $('#message').attr('username', '');
+        $('a.item').remove();
+        $('.comment').remove();
+        $('#messages-pane').css("display", "none");
+    });
+
+    socket.on('signup-status', function (data) {
+        var status = data.status;
+        if (status === 'success') {
+            var username = data.username;
+            var name = data.name;
+            $('.ui.modal')
+                .modal('hide')
+            ;
+            setUsernameAndName(username, name);
+        }
+        else {
+            showError(status, false);
+        }
+    });
+
+    socket.on('signin-status', function (data) {
+        var status = data.status;
+        if (status === 'success') {
+            console.log(data.user[0]);
+            var username = data.user[0].username;
+            var name = data.user[0].name;
+            $('.ui.modal')
+                .modal('hide')
+            ;
+            setUsernameAndName(username, name);
+            var f = data.user[0].friends;
+            var l = f.length;
+            for (var i = 0; i < l; i++) {
+                addToFriendsList(f[i]);
+            }
+        }
+        else {
+            showError(status, true);
+        }
+    });
+
+    socket.on('find-friend-status', function (data) {
+        var status = data.status;
+        if (status === 'success') {
+            var username = data.user.username;
+            var name = data.user.name;
+            addToFriendsList(username);
+            $('input').val(null);
+        }
+        else {
+            // TODO
+        }
+    });
+
+    socket.on('friend', function (data) {
+        var friend = data.friend;
+        addToFriendsList(friend);
+    });
+
+    socket.on('messages', function (data) {
+        var message = data.message;
+        messages.push(message);
+        var c = $('#message').attr('username');
+        if (c === message.to || c === message.from) {
+            addMessage(message);
+        }
+        else {
+            incrementUnseenMessages(message);
+        }
+    });
+
+    socket.on('seen-ack', function (data) {
+        var username = data.username;
+        var lastSeen = data.lastSeen;
+        var c = $('#message').attr('username');
+        if (c === username) {
+            var c = '<div class="sub header">' + lastSeen + "</div>";
+            $('#header').append(c);
+        }
+    });
+
+    socket.on('online', function (data) {
+        var username = data.username;
+        socket.emit('online-ack', {username: username});
+        setTimeout(function () {
+            if (!$('a.item[username="' + username + '"]').hasClass("green")) {
+                $('a.item[username="' + username + '"]').removeClass("blue");
+                $('a.item[username="' + username + '"]').addClass("green active");
+            }
+        }, 100);
+
+    });
+
+    socket.on('online-ack', function (data) {
+        var username = data.username;
+        if (!$('a.item[username="' + username + '"]').hasClass("green")) {
+            $('a.item[username="' + username + '"]').removeClass("blue");
+            $('a.item[username="' + username + '"]').addClass("green active");
+        }
+    });
+
+    socket.on('notonline', function (data) {
+        var username = data.username;
+        if ($('a.item[username="' + username + '"]').hasClass("green")) {
+            $('a.item[username="' + username + '"]').removeClass("green");
+            $('a.item[username="' + username + '"]').removeClass("active");
+            $('a.item[username="' + username + '"]').addClass("blue");
+        }
+    });
+
+    // when Sign Up button clicked
+    $('#signup').click(function () {
+        var username = $('#username-signup').val();
+        var name = $('#name-signup').val();
+        $('input').val(null);
+        socket.emit('signup', {username: username, name: name});
+    });
+
+    // when Sign In button clicked
+    $('#signin').click(function () {
+        var username = $('#username-signin').val();
+        $('input').val(null);
+        socket.emit('signin', {username: username});
+    });
+
+    // when the user enter name of his/her friend!
+    $('#friend-name').keyup(function (e) {
+        e.stopPropagation();
+        if (e.which == 13) {
+            var friend = $('#friend-name').val();
+            socket.emit('find-friend', {friend: friend});
+        }
+    });
+
+    // when the user enter a message
+    $('#message').keyup(function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        if (e.which == 13) {
+            var message = $('#message').val();
+            $('input').val(null);
+            var friend = $('#message').attr('username');
+            sendMessage(friend, message);
+        }
+    });
+
+    // when the user selects a friend from friends list!
+    $('a.item').click(friendSelect);
+
+    // when the user enter a message
+    $('#send').click(function (e) {
+        e.stopPropagation();
+        var message = $('#message').val();
+        $('input').val(null);
+        var friend = $('#message').attr('username');
+        sendMessage(friend, message);
+    });
+
+    // send the text to friend! (actually to server)
+    function sendMessage(friend, text) {
+        console.log(friend);
+        socket.emit('message', {'friend': friend, 'text': text});
+    }
+
+    function friendSelect(e) {
+        var t = $(e.target);
+        var fUsername = t.attr('username');
+        $('#header').text("Conversation with @" + fUsername);
+        socket.emit('seen-req', {username: fUsername});
+        $('a.item[username="' + fUsername + '"] .label').remove();
+        $('#message').attr('username', fUsername);
+        $('#messages-pane').css("display", "");
+        $('.comment').remove();
+        fillRelevantMessages(fUsername);
+    }
+
+    function fillRelevantMessages(username) {
+        var m = findRelevantMessages(username);
+        var l = m.length;
+        for (var i = 0; i < l; i++) {
+            addMessage(m[i]);
+        }
+    }
+
+    function findAnother(message) {
+        if (message.from === USERNAME)
+            return message.to;
+        else
+            return message.from;
+    }
+
+    function incrementUnseenMessages(message) {
+        if (message.from !== USERNAME && message.seen === 0) {
+            var username = message.from;
+            var x = $('a.item[username="' + username + '"] .label');
+            if (x.length === 0) {
+                var c = '<div class="ui blue label">1</div>';
+                $('a.item[username="' + username + '"]').append(c);
+            }
+            else {
+                var s = x.text();
+                var i = +s;
+                i++;
+                x.text(i + "");
+            }
+            notifyMe(message);
+        }
+    }
+
+    function findRelevantMessages(username) {
+        var m = [];
+        var l = messages.length;
+        for (var i = 0; i < l; i++) {
+            if (messages[i].from === username | messages[i].to === username) {
+                m.push(messages[i]);
+            }
+        }
+        return m;
+    }
+
+    // this function add the friend to friends list
+    var addToFriendsList = function (username) {
+        var c = '<a class="blue item" username=' + username + '>' + username + '</a>';
+        $('#roster').append(c);
+        $('a.item[username="' + username + '"]').click(friendSelect);
+    };
+
+
+    // this function set the username and name (that has given from server) in client-side!
+    var setUsernameAndName = function (username, name) {
+        USERNAME = username;
+        var $n = $('#name');
+        var s = 'Welcome, ' + name;
+        $n.text(s);
+        s = "";
+        s += '<div id="username" class="sub header">@';
+        s += username;
+        s += '</div>';
+        $n.append(s);
+    };
+
+    function showError(message, isSignIn) {
+        var c = '<div class="ui negative message"><i class="close icon"></i><div class="header">';
+        c += message;
+        c += '</div></p></div>';
+        if (isSignIn)
+            $('.horizontal.divider.ui').after(c);
+        else
+            $('#alert').prepend(c);
+    }
+
+    function addMessage(message) {
+        console.log(message);
+        var sender = message.from;
+        var date = message.dateTime;
+        var seen = message.seen;
+        var id = message._id;
+        var message = message.message;
+        if (seen === 0 && USERNAME !== sender) {
+            socket.emit('seen', {id: id, receiver: message.to});
+            message.seen = 1;
+        }
+        var c = '<div class="comment">';
+        c += '<a class="avatar">';
+        c += '<img src="static/avatars/1.jpg"></a>';
+        c += '<div class="content">';
+        c += '<a class="author">' + sender + '</a>';
+        c += '<div class="metadata"><span class="date">' + date + '</span></div>';
+        c += '<div class="text">';
+        c += message;
+        c += '</div></div></div>';
+        $('.ui.comments').append(c);
+    }
+
+    function notifyMe(message) {
+        // Let's check if the browser supports notifications
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+        }
+
+        // Let's check whether notification permissions have alredy been granted
+        else if (Notification.permission === "granted") {
+            // If it's okay let's create a notification
+            var notification = new Notification(message.from, {icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png', tag: message.from, body: message.message});
+        }
+
+        // Otherwise, we need to ask the user for permission
+        else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+                // If the user accepts, let's create a notification
+                if (permission === "granted") {
+                    var notification = new Notification(message.from, {icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png', tag: message.from, body: message.message});
+                }
+            });
+        }
+
+        // At last, if the user has denied notifications, and you
+        // want to be respectful there is no need to bother them any more.
+    }
+});
